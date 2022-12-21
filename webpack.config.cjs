0b0591777eb7a1a82e5ca9456b92edd329dc8e12
yandeu <20306025+yandeu@users.noreply.github.com>
2022-12-21 22:27:29 +0100

    Many minor changes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   mode: 'development',
   devtool: 'eval-cheap-source-map',
+  stats: 'minimal',
   entry: './client/client.js',
   output: {
     filename: 'bundle.js',
